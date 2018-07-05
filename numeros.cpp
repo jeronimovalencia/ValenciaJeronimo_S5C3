41a5ccee4d9e868689142233a2bb51bdeb4eddda
Jeronimo Valencia <j.valencia11@uniandes.edu.co>
2018-07-05 11:01:31 -0500

    Imprime 25 números pares menores a 89 de una lista de 1000 numeros aleatorios

--- numeros.cpp
+++ numeros.cpp
@@ -8,11 +8,17 @@ using std::endl;
 
 int main(){
   int i, numeroal;
+  int cont = 0;
   for(i=0;i<1000;i++){
     numeroal = rand() % 100 + 1;
-    cout << i << " " << numeroal << endl;  	 	 
+    if(numeroal%2 == 0 && numeroal < 89 && cont < 25)
+	{
+		cout << "Número '" << cont + 1 << "' menor a 89" << endl;
+		cout << numeroal << endl;
+		cont++;
+		if(cont == 25) {break;}
+	}	 	 
   }	
-
   return 0;
 }
 
