ff02b4c0b4b2ca1d23260b57eb62aa4d19c7cf76
Jeronimo Valencia <j.valencia11@uniandes.edu.co>
2018-07-05 11:10:42 -0500

    Script para ejecutar holamundo, multiplicar y numeros (.cpp)

---
+++ ValenciaJeronimo.sh
@@ -0,0 +1,15 @@
+echo Programa Hola Mundo
+g++ holamundo.cpp -o hola.exe
+./hola.exe
+rm hola.exe
+echo 
+echo Programa Multiplicar
+g++ multiplicar.cpp -o multipl.exe
+./multipl.exe
+rm multipl.exe
+echo
+echo Programa Numeros Aleatorios
+g++ numeros.cpp -o nums.exe
+./nums.exe
+rm nums.exe
+
